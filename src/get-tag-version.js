fc13977d98ad73a6f0020407b7ea5b38de73a0fa
Kelly Selden <kellyselden@gmail.com>
2017-08-03 15:05:04 -0700

    rename to distTag

--- src/get-tag-version.js
+++ src/get-tag-version.js
@@ -2,9 +2,9 @@
 
 const run = require('./run');
 
-module.exports = function getTagVersion(tag) {
+module.exports = function getTagVersion(distTag) {
   let version = JSON.parse(
-    run(`npm info ember-cli@${tag} version --json`)
+    run(`npm info ember-cli@${distTag} version --json`)
   );
 
   return `v${version}`;
