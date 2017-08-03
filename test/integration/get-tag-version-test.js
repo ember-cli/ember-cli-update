4378857078c4fe8c13ad7dfee7edeba15f8a66fc
Kelly Selden <kellyselden@gmail.com>
2017-08-03 14:42:41 -0700

    check for valid semver strings

--- test/integration/get-tag-version-test.js
+++ test/integration/get-tag-version-test.js
@@ -2,12 +2,13 @@
 
 const expect = require('chai').expect;
 const getTagVersion = require('../../src/get-tag-version');
+const semver = require('semver');
 
 describe('Integration - getTagVersion', function() {
   this.timeout(10000);
 
   it('works', function() {
-    expect(getTagVersion('beta')).to.not.be.empty;
-    expect(getTagVersion('latest')).to.not.be.empty;
+    expect(semver.valid(getTagVersion('beta'))).to.not.be.null;
+    expect(semver.valid(getTagVersion('latest'))).to.not.be.null;
   });
 });
