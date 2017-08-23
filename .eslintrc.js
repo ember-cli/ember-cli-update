401a3fa34957a45abb1dfc98444bb815974aec99
Kelly Selden <kellyselden@gmail.com>
2017-08-23 11:34:37 -0700

    prevent accidental .only in tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,15 @@ module.exports = {
     ecmaVersion: 2017,
     sourceType: 'module'
   },
+  plugins: [
+    'mocha'
+  ],
   extends: 'sane',
   env: {
     es6: true,
     node: true
   },
   rules: {
+    'mocha/no-exclusive-tests': 'error'
   }
 };
