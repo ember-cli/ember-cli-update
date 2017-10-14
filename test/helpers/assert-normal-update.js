3eaaaf9506a629118346c06619c63d1975ed1443
Kelly Selden <kellyselden@gmail.com>
2017-10-14 13:24:34 -0700

    use porcelain status in tests

--- test/helpers/assert-normal-update.js
+++ test/helpers/assert-normal-update.js
@@ -4,14 +4,18 @@ const expect = require('chai').expect;
 
 module.exports = function assertNormalUpdate(status) {
   // changed locally, no change upstream
-  expect(status).to.not.contain(' .ember-cli');
+  expect(status).to.not.contain(` .ember-cli
+`);
 
   // exists locally, also added upstream with changes
-  expect(status).to.contain('modified:   .eslintrc.js');
+  expect(status).to.contain(`M  .eslintrc.js
+`);
 
   // changed locally, removed upstream
-  expect(status).to.contain('deleted:    bower.json');
+  expect(status).to.contain(`D  bower.json
+`);
 
   // changed locally, also changed upstream
-  expect(status).to.contain('modified:   README.md');
+  expect(status).to.contain(`M  README.md
+`);
 };
