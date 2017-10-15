316696b4090c1d6250625ce8ab771602fee35b9b
Kelly Selden <kellyselden@gmail.com>
2017-10-15 12:29:23 -0700

    add ignoreConflicts test

--- test/integration/index-test.js
+++ test/integration/index-test.js
@@ -1,7 +1,9 @@
 'use strict';
 
+const path = require('path');
 const expect = require('chai').expect;
 const tmp = require('tmp');
+const fs = require('fs');
 const gitFixtures = require('git-fixtures');
 const isGitClean = require('git-diff-apply').isGitClean;
 const emberCliUpdate = require('../../src');
@@ -32,6 +34,7 @@ describe('Integration - index', function() {
   function merge(options) {
     let fixturesPath = options.fixturesPath;
     let dirty = options.dirty;
+    let ignoreConflicts = options.ignoreConflicts;
 
     buildTmp(
       fixturesPath,
@@ -43,7 +46,8 @@ describe('Integration - index', function() {
     process.chdir(tmpPath);
 
     let promise = emberCliUpdate({
-      to: '2.16.0-beta.2'
+      to: '2.16.0-beta.2',
+      ignoreConflicts
     });
 
     return processExit({
@@ -67,6 +71,22 @@ describe('Integration - index', function() {
 
       expect(stderr).to.contain('You must start with a clean working directory');
       expect(stderr).to.not.contain('UnhandledPromiseRejectionWarning');
+    });
+  });
+
+  it('handles ignoreConflicts', function() {
+    return merge({
+      fixturesPath: 'test/fixtures/local/my-app',
+      ignoreConflicts: true
+    }).then(result => {
+      let status = result.status;
+
+      let actual = fs.readFileSync(path.join(tmpPath, '.eslintrc.js'), 'utf8');
+
+      expect(actual).to.contain('<<<<<<< HEAD');
+
+      expect(status).to.match(/^AA \.eslintrc\.js$/m);
+      expect(status).to.match(/^UD bower\.json$/m);
     });
   });
 
