3049c0ac894ea1a05c9d8fa86ff4e8ff8f854279
Kelly Selden <kellyselden@gmail.com>
2017-11-02 21:34:23 -0700

    add package version tests

--- test/integration/get-package-version-test.js
+++ test/integration/get-package-version-test.js
@@ -4,7 +4,15 @@ const expect = require('chai').expect;
 const getPackageVersion = require('../../src/get-package-version');
 
 describe('Integration - getPackageVersion', function() {
-  it('works', function() {
-    expect(getPackageVersion('test/fixtures/version')).to.equal('2.11');
+  it('returns falsy for no devDependencies', function() {
+    expect(getPackageVersion('test/fixtures/version/no-dev-deps')).to.be.not.ok;
+  });
+
+  it('returns falsy for no ember-cli', function() {
+    expect(getPackageVersion('test/fixtures/version/no-ember-cli')).to.be.not.ok;
+  });
+
+  it('works with only ember-cli', function() {
+    expect(getPackageVersion('test/fixtures/version/ember-cli')).to.equal('2.11');
   });
 });
