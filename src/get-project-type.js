3313f2890ba8be18e7e60ef28ab7b4ad36a41a5c
Kelly Selden <kellyselden@gmail.com>
2017-11-03 19:55:30 -0700

    more tests

--- src/get-project-type.js
+++ src/get-project-type.js
@@ -6,7 +6,19 @@ const path = require('path');
 module.exports = function getProjectType(projectPath) {
   let packagePath = path.join(projectPath, 'package.json');
 
-  let packageJson = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
+  let packageJson;
+
+  try {
+    packageJson = fs.readFileSync(packagePath, 'utf8');
+  } catch (err) {
+    throw 'No package.json was found in this directory';
+  }
+
+  try {
+    packageJson = JSON.parse(packageJson);
+  } catch (err) {
+    throw 'The package.json is malformed';
+  }
 
   let keywords = packageJson.keywords;
 
--- test/integration/get-package-version-test.js
+++ test/integration/get-package-version-test.js
@@ -4,6 +4,18 @@ const expect = require('chai').expect;
 const getPackageVersion = require('../../src/get-package-version');
 
 describe('Integration - getPackageVersion', function() {
+  it('throws if no package.json', function() {
+    expect(() => {
+      getPackageVersion('test/fixtures/no-package-json');
+    }).to.throw('No package.json was found in this directory');
+  });
+
+  it('throws if malformed package.json', function() {
+    expect(() => {
+      getPackageVersion('test/fixtures/malformed-package-json');
+    }).to.throw('The package.json is malformed');
+  });
+
   it('returns falsy for no devDependencies', function() {
     expect(getPackageVersion('test/fixtures/version/no-dev-deps')).to.be.not.ok;
   });
