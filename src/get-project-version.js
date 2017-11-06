46c306f93d71046785e0d3d668df255eafd53934
Kelly Selden <kellyselden@gmail.com>
2017-11-05 23:44:24 -0800

    require --from if glimmer < 0.6.3

--- src/get-project-version.js
+++ src/get-project-version.js
@@ -2,8 +2,14 @@
 
 const semver = require('semver');
 
-module.exports = function getProjectVersion(packageVersion, versions) {
+const glimmerVersionCutoff = '0.6.3';
+
+module.exports = function getProjectVersion(packageVersion, versions, projectType) {
   let projectVersion = semver.minSatisfying(versions, packageVersion);
+
+  if (projectType === 'glimmer' && semver.lt(projectVersion, glimmerVersionCutoff)) {
+    throw `This Glimmer app was generated using a blueprint version older than v${glimmerVersionCutoff}, and therefore the version cannot be determined. Please use the "--from" option to indicate your starting version.`;
+  }
 
   return projectVersion;
 };
