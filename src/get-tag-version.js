527e6cdfca150a9798ab2a2f3ace5b731d4600e2
Jeremy Bertrand <jeremy.bertrand@6cure.com>
2017-11-08 15:39:42 +0100

    Default npm package to ember-cli Fix tests

--- src/get-tag-version.js
+++ src/get-tag-version.js
@@ -26,12 +26,11 @@ module.exports = function getTagVersion(to, versions, projectType) {
     let pkg;
 
     switch (projectType) {
-      case 'app':
-      case 'addon':
-        pkg = 'ember-cli';
-        break;
       case 'glimmer':
         pkg = '@glimmer/blueprint';
+        break;
+      default:
+        pkg = 'ember-cli';
         break;
     }
 
