fc5447449ced377d305eb8008c737402478fd1dd
Kelly Selden <kellyselden@gmail.com>
2017-12-10 17:13:22 -0800

    fix reset command

--- bin/ember-cli-update.js
+++ bin/ember-cli-update.js
@@ -12,10 +12,12 @@ const from = argv['from'];
 const to = argv['to'];
 const resolveConflicts = argv['resolve-conflicts'];
 const runCodemods = argv['run-codemods'];
+const reset = argv['reset'];
 
 emberCliUpdate({
   from,
   to,
   resolveConflicts,
-  runCodemods
+  runCodemods,
+  reset
 }).catch(console.error);
