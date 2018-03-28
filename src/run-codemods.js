8186cd0d0478c0cc5ef59d8ccc69f8c8739c1e8b
Kelly Selden <kellyselden@gmail.com>
2018-03-28 09:14:21 -0700

    chain promises instead of running all at once

--- src/run-codemods.js
+++ src/run-codemods.js
@@ -20,12 +20,14 @@ module.exports = function runCodemods(options) {
   let projectType = options.projectType;
   let startVersion = options.startVersion;
 
-  return Promise.all(Object.keys(codemods).filter(codemod => {
+  return Object.keys(codemods).filter(codemod => {
     return semver.gte(startVersion, codemods[codemod].version) &&
       codemods[codemod].projectTypes.indexOf(projectType) !== -1;
-  }).reduce((promises, codemod) => {
-    return promises.concat(codemods[codemod].commands.map(utils.npx));
-  }, [])).then(() => {
+  }).reduce((promise, codemod) => {
+    return codemods[codemod].commands.reduce((promise, command) => {
+      return promise.then(() => utils.npx(command));
+    }, promise);
+  }, Promise.resolve()).then(() => {
     return utils.run('git add -A');
   });
 };
