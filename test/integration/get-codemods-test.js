a34db6684809eb42cdb931f0ff9410d186784d89
Kelly Selden <kellyselden@gmail.com>
2018-04-10 14:07:07 -0700

    increase timeouts

--- test/integration/get-codemods-test.js
+++ test/integration/get-codemods-test.js
@@ -4,6 +4,8 @@ const expect = require('chai').expect;
 const getCodemods = require('../../src/get-codemods');
 
 describe('Integration - getCodemods', function() {
+  this.timeout(5000);
+
   it('gets codemods', function() {
     return getCodemods().then(codemods => {
       expect(codemods).to.be.an.instanceof(Object);
