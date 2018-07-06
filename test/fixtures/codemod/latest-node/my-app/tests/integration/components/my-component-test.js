b275ffb0aa4e076aee5bb7713ba824c3e2343040
Kelly Selden <kellyselden@gmail.com>
2018-07-06 14:46:18 +0100

    update to new qunit-dom-codemod

--- test/fixtures/codemod/latest-node/my-app/tests/integration/components/my-component-test.js
+++ test/fixtures/codemod/latest-node/my-app/tests/integration/components/my-component-test.js
@@ -13,7 +13,7 @@ module('Integration | Component | my component', function(hooks) {
 
     await render(hbs`{{my-component}}`);
 
-    assert.equal(find('*').textContent.trim(), '');
+    assert.dom('*').hasText('');
 
     // Template block usage:
     await render(hbs`
@@ -22,6 +22,6 @@ module('Integration | Component | my component', function(hooks) {
       {{/my-component}}
     `);
 
-    assert.equal(find('*').textContent.trim(), 'template block text');
+    assert.dom('*').hasText('template block text');
   });
 });
