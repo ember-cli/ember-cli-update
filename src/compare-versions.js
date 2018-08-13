3d8904ddff6f8918a760f87b674d3bbd20e356a0
Kelly Selden <kellyselden@gmail.com>
2018-08-13 14:45:22 -0700

    add comment

--- src/compare-versions.js
+++ src/compare-versions.js
@@ -9,6 +9,9 @@ module.exports = function compareVersions(options) {
 
   let compareUrl = `${remoteUrl}/compare/${startTag}...${endTag}`;
 
+  // even though this returns a promise, we don't want to use
+  // it because it blocks the process
+  // we want to open the browser and exit
   utils.opn(compareUrl, {
     // this means it no longer returns a promise
     wait: false
