1fd575d8c851e301814f0e473d55afbb3dc17a05
Kelly Selden <kellyselden@gmail.com>
2019-01-01 13:51:18 +0000

    start using boilerplate-update

--- src/utils.js
+++ src/utils.js
@@ -4,11 +4,10 @@ const denodeify = require('denodeify');
 
 module.exports.run = require('./run');
 module.exports.npx = require('./npx');
-module.exports.getCodemods = require('./get-codemods');
+module.exports.getCodemods = require('boilerplate-update/src/get-codemods').bind(null, 'https://rawgit.com/ember-cli/ember-cli-update-codemods-manifest/v2/manifest.json');
 module.exports.getNodeVersion = require('./get-node-version');
 module.exports.opn = require('opn');
 module.exports.getApplicableCodemods = require('./get-applicable-codemods');
-module.exports.runCodemod = require('./run-codemod');
 module.exports.resolve = denodeify(require('resolve'));
 module.exports.require = require;
 module.exports.which = denodeify(require('which'));
