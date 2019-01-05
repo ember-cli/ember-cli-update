5b6ab3e0e4b5a890955e467b60cbfd99745b22ce
Kelly Selden <kellyselden@gmail.com>
2019-01-05 17:37:54 +0000

    remove local npx

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 'use strict';
 
 module.exports.run = require('./run');
-module.exports.npx = require('./npx');
+module.exports.npx = require('boilerplate-update/src/npx');
 module.exports.opn = require('opn');
 module.exports.getVersions = require('boilerplate-update/src/get-versions');
