b5d997fa965a830fbf56672bb3d6fe07843926b3
Kelly Selden <kellyselden@gmail.com>
2019-01-22 14:47:41 +0000

    update boilerplate-update

--- test/unit/get-project-version-test.js
+++ test/unit/get-project-version-test.js
@@ -4,24 +4,6 @@ const { expect } = require('chai');
 const getProjectVersion = require('../../src/get-project-version');
 
 describe('Unit - getProjectVersion', function() {
-  it('works', function() {
-    expect(getProjectVersion(
-      '2.11.1',
-      [
-        '2.11.0',
-        '2.11.1',
-        '2.11.2'
-      ]
-    )).to.equal('2.11.1');
-    expect(getProjectVersion(
-      '2.12',
-      [
-        '2.12.0',
-        '2.12.1'
-      ]
-    )).to.equal('2.12.0');
-  });
-
   it('throws if glimmer and < 0.6.3', function() {
     expect(() => getProjectVersion(
       '0.3',
