98024a654eb061695fc60b498a8030bb551c7574
Kelly Selden <kellyselden@gmail.com>
2019-03-08 18:20:34 +0000

    use new mergeOptions

--- src/index.js
+++ src/index.js
@@ -2,18 +2,14 @@
 
 const co = require('co');
 const utils = require('./utils');
-const getPackageJson = require('boilerplate-update/src/get-package-json');
 const getProjectOptions = require('./get-project-options');
 const getPackageName = require('./get-package-name');
 const getPackageVersion = require('./get-package-version');
 const getProjectVersion = require('./get-project-version');
 const _getTagVersion = require('./get-tag-version');
 const getRemoteUrl = require('./get-remote-url');
-const listCodemods = require('boilerplate-update/src/list-codemods');
 const boilerplateUpdate = require('boilerplate-update');
 const getStartAndEndCommands = require('./get-start-and-end-commands');
-
-const codemodsUrl = 'https://raw.githubusercontent.com/ember-cli/ember-cli-update-codemods-manifest/v3/manifest.json';
 
 module.exports = co.wrap(function* emberCliUpdate({
   from,
@@ -23,56 +19,45 @@ module.exports = co.wrap(function* emberCliUpdate({
   reset,
   compareOnly,
   statsOnly,
-  listCodemods: _listCodemods,
+  listCodemods,
   createCustomDiff,
   wasRunAsExecutable
 }) {
-  if (_listCodemods) {
-    return yield listCodemods(codemodsUrl);
-  }
-
-  let packageJson = yield getPackageJson();
-  let projectOptions = getProjectOptions(packageJson);
-  let packageName = getPackageName(projectOptions);
-  let packageVersion = getPackageVersion(packageJson, packageName);
-  let versions = yield utils.getVersions(packageName);
-  let getTagVersion = _getTagVersion(versions, packageName);
-
-  let startVersion;
-  if (from) {
-    startVersion = yield getTagVersion(from);
-  } else {
-    startVersion = getProjectVersion(packageVersion, versions, projectOptions);
-  }
-
-  let endVersion = yield getTagVersion(to);
-
-  let remoteUrl = getRemoteUrl(projectOptions);
-
-  let customDiffOptions;
-  if (createCustomDiff) {
-    customDiffOptions = getStartAndEndCommands({
-      projectName: packageJson.name,
-      projectOptions,
-      startVersion,
-      endVersion
-    });
-  }
-
   return yield (yield boilerplateUpdate({
-    remoteUrl,
+    projectOptions: ({ packageJson }) => getProjectOptions(packageJson),
+    mergeOptions: co.wrap(function* mergeOptions({
+      packageJson,
+      projectOptions
+    }) {
+      let packageName = getPackageName(projectOptions);
+      let packageVersion = getPackageVersion(packageJson, packageName);
+      let versions = yield utils.getVersions(packageName);
+      let getTagVersion = _getTagVersion(versions, packageName);
+
+      let startVersion;
+      if (from) {
+        startVersion = yield getTagVersion(from);
+      } else {
+        startVersion = getProjectVersion(packageVersion, versions, projectOptions);
+      }
+
+      let endVersion = yield getTagVersion(to);
+
+      return {
+        startVersion,
+        endVersion
+      };
+    }),
+    remoteUrl: ({ projectOptions }) => getRemoteUrl(projectOptions),
     compareOnly,
     resolveConflicts,
     reset,
     statsOnly,
+    listCodemods,
     runCodemods,
-    codemodsUrl,
-    packageJson,
-    projectOptions,
-    startVersion,
-    endVersion,
+    codemodsUrl: 'https://raw.githubusercontent.com/ember-cli/ember-cli-update-codemods-manifest/v3/manifest.json',
     createCustomDiff,
-    customDiffOptions,
+    customDiffOptions: getStartAndEndCommands,
     wasRunAsExecutable
   })).promise;
 });
