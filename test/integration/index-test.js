080648ff448171edce73f41a37337b1efed42229
Kelly Selden <kellyselden@gmail.com>
2019-04-28 15:11:10 +0100

    fix assertion

--- test/integration/index-test.js
+++ test/integration/index-test.js
@@ -214,7 +214,8 @@ describe(function() {
 
       expect(result, 'don\'t accidentally print anything to the console').to.be.undefined;
 
-      expect(opn).to.have.been.calledOnceWith('https://github.com/ember-cli/ember-new-output/compare/v2.11.1...v3.2.0-beta.1');
+      expect(opn).to.have.been.calledOnce
+        .and.to.have.been.calledWith('https://github.com/ember-cli/ember-new-output/compare/v2.11.1...v3.2.0-beta.1');
     });
   });
 
