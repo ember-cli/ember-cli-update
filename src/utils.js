28cd4f215d616d9aff52f4cf098da3704dd9f6e7
Kelly Selden <kellyselden@gmail.com>
2019-04-30 20:36:19 +0100

    run isn't used in utils

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,5 @@
 'use strict';
 
-module.exports.run = require('./run');
 module.exports.npx = require('boilerplate-update/src/npx');
 module.exports.getVersions = require('boilerplate-update/src/get-versions');
 
