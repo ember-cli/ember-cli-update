682d7bbd282f0028f4467693c41e9142fc1652d6
Kelly Selden <kellyselden@gmail.com>
2019-09-07 10:47:29 +0100

    fix loadBlueprintFile typo

--- src/load-blueprint-file.js
+++ src/load-blueprint-file.js
@@ -3,10 +3,10 @@
 const fs = require('fs-extra');
 const path = require('path');
 
-async function loadSafeBlueprintFile(cwd) {
+async function loadBlueprintFile(cwd) {
   try {
     return await fs.readJson(path.join(cwd, 'config/ember-cli-update.json'));
   } catch (err) {}
 }
 
-module.exports = loadSafeBlueprintFile;
+module.exports = loadBlueprintFile;
