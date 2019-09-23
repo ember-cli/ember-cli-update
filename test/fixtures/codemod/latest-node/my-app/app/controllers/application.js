714b989c2feb9938b15418cab6119bc3d110b488
Kelly Selden <kellyselden@gmail.com>
2019-09-23 16:06:51 +0100

    test notify-property-change codemod

--- test/fixtures/codemod/latest-node/my-app/app/controllers/application.js
+++ test/fixtures/codemod/latest-node/my-app/app/controllers/application.js
@@ -1,4 +1,14 @@
 import Controller from '@ember/controller';
+import Ember from 'ember';
 
 export default Controller.extend({
+  actions: {
+    foo(object) {
+      this.doStuff(object);
+      Ember.notifyPropertyChange(object, 'someProperty');
+
+      this.doStuff(object);
+      object.notifyPropertyChange('someProperty');
+    }
+  }
 });
--- test/fixtures/codemod/local/my-app/app/controllers/application.js
+++ test/fixtures/codemod/local/my-app/app/controllers/application.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    foo(object) {
+      Ember.propertyWillChange(object, 'someProperty');
+      this.doStuff(object);
+      Ember.propertyDidChange(object, 'someProperty');
+
+      object.propertyWillChange('someProperty');
+      this.doStuff(object);
+      object.propertyDidChange('someProperty');
+    }
+  }
 });
