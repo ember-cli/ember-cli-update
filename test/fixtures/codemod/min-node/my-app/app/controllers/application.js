8ccc76d62186b53020615e36835c3cb12298fced
Rajasegar Chandran <rajasegar.c@gmail.com>
2019-09-29 19:22:26 +0530

    [CHORE] Changing single quotes to double

--- test/fixtures/codemod/min-node/my-app/app/controllers/application.js
+++ test/fixtures/codemod/min-node/my-app/app/controllers/application.js
@@ -1,20 +1,20 @@
-import Controller from '@ember/controller';
-import Ember from 'ember';
-import { assign } from '@ember/polyfills';
+import Controller from "@ember/controller";
+import Ember from "ember";
+import { assign } from "@ember/polyfills";
 
 export default Controller.extend({
   actions: {
     foo(object) {
       this.doStuff(object);
-      Ember.notifyPropertyChange(object, 'someProperty');
+      Ember.notifyPropertyChange(object, "someProperty");
 
       this.doStuff(object);
-      object.notifyPropertyChange('someProperty');
+      object.notifyPropertyChange("someProperty");
     },
 
     bar() {
-      var a = { first: 'Yehuda' };
-      var b = { last: 'Katz' };
+      var a = { first: "Yehuda" };
+      var b = { last: "Katz" };
       assign(a, b); // a == { first: 'Yehuda', last: 'Katz' }, b == { last: 'Katz' }
     }
   }
