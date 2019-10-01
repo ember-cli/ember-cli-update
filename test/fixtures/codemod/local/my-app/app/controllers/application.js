2e73342c4a169f211ec1d5005d926497e55005d8
Rajasegar Chandran <rajasegar.c@gmail.com>
2019-10-01 07:56:15 +0530

    [BUGFIX] Changing everything to single quotes

--- test/fixtures/codemod/local/my-app/app/controllers/application.js
+++ test/fixtures/codemod/local/my-app/app/controllers/application.js
@@ -1,21 +1,21 @@
-import Ember from "ember";
-import { merge } from "@ember/polyfills";
+import Ember from 'ember';
+import { merge } from '@ember/polyfills';
 
 export default Ember.Controller.extend({
   actions: {
     foo(object) {
-      Ember.propertyWillChange(object, "someProperty");
+      Ember.propertyWillChange(object, 'someProperty');
       this.doStuff(object);
-      Ember.propertyDidChange(object, "someProperty");
+      Ember.propertyDidChange(object, 'someProperty');
 
-      object.propertyWillChange("someProperty");
+      object.propertyWillChange('someProperty');
       this.doStuff(object);
-      object.propertyDidChange("someProperty");
+      object.propertyDidChange('someProperty');
     },
 
     bar() {
-      var a = { first: "Yehuda" };
-      var b = { last: "Katz" };
+      var a = { first: 'Yehuda' };
+      var b = { last: 'Katz' };
       merge(a, b); // a == { first: 'Yehuda', last: 'Katz' }, b == { last: 'Katz' }
     }
   }
