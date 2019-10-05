7da8696cf3ce61e53da797a604f1039d9da89589
Rajasegar Chandran <rajasegar.c@gmail.com>
2019-10-05 19:27:10 +0530

    [FEAT] Test deprecate-router-events codemod

---
+++ test/fixtures/codemod/min-node/my-app/app/routes/index.js
@@ -0,0 +1,22 @@
+import Router from '@ember/routing/router';
+import { inject as service } from '@ember/service';
+
+export default Router.extend({
+  currentUser: service('current-user'),
+
+  init() {
+    this._super(...arguments);
+    this.on('routeWillChange', transition => {
+      if (!this.currentUser.isLoggedIn) {
+        transition.abort();
+        this.transitionTo('login');
+      }
+    });
+
+    this.on('routeDidChange', transition => {
+      ga.send('pageView', {
+        pageName: transition.to.name
+      });
+    });
+  }
+});
