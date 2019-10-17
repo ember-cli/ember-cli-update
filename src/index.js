d2c9b6469079dc5dc8cee8831f2481cbf72ccb5f
Rajasegar Chandran <rajasegar.c@gmail.com>
2019-10-17 11:35:14 +0530

    [FEAT] Add cp-volatile codemod

--- src/index.js
+++ src/index.js
@@ -194,7 +194,7 @@ All blueprints are up-to-date!`;
     statsOnly,
     listCodemods,
     runCodemods,
-    codemodsUrl: 'https://raw.githubusercontent.com/ember-cli/ember-cli-update-codemods-manifest/v3/manifest.json',
+    codemodsUrl: 'https://raw.githubusercontent.com/rajasegar/ember-cli-update-codemods-manifest/cp-volatile/manifest.json',
     createCustomDiff,
     ignoredFiles: [await getBlueprintFilePath(cwd)],
     wasRunAsExecutable
