0b57151234812588a80efc2d9270afadef9ac8d3
Kelly Selden <kellyselden@gmail.com>
2019-12-07 23:22:18 +0000

    remove unnecessary init code

--- src/init.js
+++ src/init.js
@@ -1,18 +1,15 @@
 'use strict';
 
-const getProjectOptions = require('./get-project-options');
 const boilerplateUpdate = require('boilerplate-update');
 const getStartAndEndCommands = require('./get-start-and-end-commands');
 const parseBlueprintPackage = require('./parse-blueprint-package');
 const downloadPackage = require('./download-package');
 const saveBlueprint = require('./save-blueprint');
-const loadDefaultBlueprint = require('./load-default-blueprint');
 const loadDefaultBlueprintFromDisk = require('./load-default-blueprint-from-disk');
 const loadSafeBlueprint = require('./load-safe-blueprint');
 const loadSafeBlueprintFile = require('./load-safe-blueprint-file');
 const stageBlueprintFile = require('./stage-blueprint-file');
 const getBlueprintFilePath = require('./get-blueprint-file-path');
-const isDefaultBlueprint = require('./is-default-blueprint');
 const loadBlueprintFile = require('./load-blueprint-file');
 const bootstrap = require('./bootstrap');
 const findBlueprint = require('./find-blueprint');
@@ -75,27 +72,17 @@ module.exports = async function init({
 
   blueprint.version = downloadedPackage.version;
 
-  let isCustomBlueprint = !isDefaultBlueprint(blueprint);
-
   let result = await (await boilerplateUpdate({
-    projectOptions: ({ packageJson }) => getProjectOptions(packageJson, blueprint),
     endVersion: blueprint.version,
     resolveConflicts,
     reset,
     createCustomDiff: true,
     customDiffOptions: ({
+      packageJson
+    }) => getStartAndEndCommands({
       packageJson,
-      projectOptions
-    }) => {
-      if (!isCustomBlueprint) {
-        blueprint = loadDefaultBlueprint(projectOptions, blueprint.version);
-      }
-
-      return getStartAndEndCommands({
-        packageJson,
-        endBlueprint: blueprint
-      });
-    },
+      endBlueprint: blueprint
+    }),
     ignoredFiles: [await getBlueprintFilePath(cwd)],
     wasRunAsExecutable
   })).promise;
