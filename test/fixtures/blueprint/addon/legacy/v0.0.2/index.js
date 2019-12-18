c828a82245557404305f6574c01d2a5025a31490
Kelly Selden <kellyselden@gmail.com>
2019-12-18 22:37:24 +0000

    failing test for peer deps in an addon or blueprint

--- test/fixtures/blueprint/addon/legacy/v0.0.2/index.js
+++ test/fixtures/blueprint/addon/legacy/v0.0.2/index.js
@@ -1,5 +1,11 @@
 'use strict';
 
+// Test that peer-deps, whether explicit or implicit,
+// don't throw, for instance if we try to download to tmp
+// and `npm install <folder>`, which will symlink.
+// https://github.com/salsify/ember-cli-dependency-lint/blob/v1.0.3/lib/commands/dependency-lint.js#L5
+require('ember-cli/lib/models/command');
+
 module.exports = {
   name: require('./package').name
 };
