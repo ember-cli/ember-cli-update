a713b668856d34b57b78c230c0ef20083e1392d1
Kelly Selden <kellyselden@gmail.com>
2020-01-15 22:28:19 +0000

    @kellyselden/node-template

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,12 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2018
   },
-  extends: [
-    'sane-node'
-  ],
   env: {
     es6: true
   },
-  rules: {
-    // https://github.com/eslint/eslint/issues/11899
-    'require-atomic-updates': 'off'
-  },
+  extends: [
+    'sane-node'
+  ],
   overrides: [
     {
       files: ['bin/**/*.js'],
@@ -21,20 +17,22 @@ module.exports = {
       }
     },
     {
-      files: ['test/**/*-test.js'],
+      files: [
+        'test/**/*-test.js'
+      ],
+      env: {
+        mocha: true
+      },
       plugins: [
         'mocha'
       ],
       extends: [
         'plugin:mocha/recommended'
       ],
-      env: {
-        mocha: true
-      },
       rules: {
+        'mocha/no-exclusive-tests': 'error',
         'mocha/no-setup-in-describe': 'off',
-        'mocha/no-hooks-for-single-case': 'off',
-        'mocha/no-exclusive-tests': 'error'
+        'mocha/no-hooks-for-single-case': 'off'
       }
     }
   ]
