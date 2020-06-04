36921d10761a51abb54a952fa78167a7e964088f
NullVoxPopuli <LPSego3+dev@gmail.com>
2020-06-04 11:16:23 -0400

    support local paths

--- src/normalize-blueprint-args.js
+++ src/normalize-blueprint-args.js
@@ -1,16 +1,33 @@
 'use strict';
 
+const path = require('path');
+const fs = require('fs');
+
 function normalizeBlueprintArgs({
   packageName,
   blueprintName
 }) {
+  let localLocation;
+
   if (!packageName) {
-    packageName = blueprintName;
+    let potentialLocalPath = path.join(
+      process.cwd(),
+      blueprintName,
+      'package.json'
+    );
+
+    if (fs.existsSync(potentialLocalPath)) {
+      packageName = require(potentialLocalPath).name;
+      localLocation = potentialLocalPath;
+    } else {
+      packageName = blueprintName;
+    }
   }
 
   return {
     packageName,
-    blueprintName
+    blueprintName,
+    localLocation
   };
 }
 
