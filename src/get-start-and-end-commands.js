a10f471d5eefa4d3d632d69ed9a63a1a0d7c7f37
Chris Manson <chris@manson.ie>
2023-08-10 17:34:55 +0100

    use latest ember-cli with a custom blueprint

--- src/get-start-and-end-commands.js
+++ src/get-start-and-end-commands.js
@@ -174,7 +174,7 @@ function runEmberRemotely({
   let isGlimmer = blueprint.packageName === glimmerPackageName && blueprint.name === glimmerPackageName;
 
   if (isCustomBlueprint || isGlimmer) {
-    args = [`ember-cli@${lastNode8Version}`, ...args];
+    args = ['ember-cli@latest', ...args];
     // args = ['-p', 'github:ember-cli/ember-cli#cfb9780', 'ember', 'new', projectName, `-dir=${directoryName}, '-sg', -sn', '-b', `${blueprint.packageName}@${blueprint.version}`];
   } else {
     args = ['-p', `ember-cli@${blueprint.version}`, 'ember', ...args];
