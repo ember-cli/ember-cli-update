a995ebec2212fb8ba100421538789e1373fc47fb
Chris Manson <chris@manson.ie>
2023-08-25 15:48:25 +0100

    fix npm package download logic using pacote

--- src/download-package.js
+++ src/download-package.js
@@ -2,8 +2,8 @@
 
 const path = require('path');
 const { createTmpDir } = require('./tmp');
-const npa = require('npm-package-arg');
-const { spawn } = require('./run');
+const pacote = require('pacote');
+const { mkdir } = require('fs/promises');
 
 /**
  * Download the package in a temporary location and read the contents of the package.json
@@ -25,21 +25,19 @@ async function downloadPackage(name, url, range) {
   }
 
   let newTmpDir = await createTmpDir();
-  let { stdout } = await spawn('npm', ['install', url], { cwd: newTmpDir });
-  // if (!name) {
-  //   name = stdout.match(/^\+ (.*)@\d\.\d\.\d.*$/m)[1];
-  // }
-  let arg = stdout.match(/^\+ (.*)$/m)[1];
-  let parsed = npa(arg);
-  name = parsed.name;
-  let version = parsed.rawSpec;
-  let _path = path.join(newTmpDir, 'node_modules', name);
-  // let version = require(path.join(_path, 'package')).version;
+
+  let manifest = await pacote.manifest(url);
+
+  let _path = path.join(newTmpDir, manifest.name);
+
+  await mkdir(_path);
+
+  await pacote.extract(url, _path);
 
   return {
-    name,
+    name: manifest.name,
     path: _path,
-    version
+    version: manifest.version
   };
 }
 
